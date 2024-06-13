1102f8a86b1ac78204196878e2919d75d193e166
wdchocopie <81132394+wdchocopie@users.noreply.github.com>
2024-06-14 01:33:10 +0700

    Update index.php

--- HW-SUN-1/index.php
+++ HW-SUN-1/index.php
@@ -22,30 +22,33 @@
                 $username = $_POST['username'];
                 $password = $_POST['password'];
 
-                //account array
-                //admin account
-                $admin = array("carlos" => "12345678");
-                //user account
-                $user = array("peter" => "4321");
+                //accounts array
+                $accounts = array(
+                    "wdchocopie" => array("role" => "admin", "password" => "12345678"),
+                    "tranlong" => array("role" => "user", "password" => "4321")
+                );
 
                 //check if username and password is empty
                 if (empty($username) || empty($password)){
                     echo "<br>Username or Password is empty";
                 } else {
-                    //check if username and password is valid
-                    if (array_key_exists($username, $admin) && $password == $admin[$username]){
+                    //check if username exists and password is correct
+                    if (array_key_exists($username, $accounts) && $password == $accounts[$username]['password']){
                         echo "<br>Login successful";
-                        echo "<br><h4>Welcome Admin</h4>";
-                        echo "User Account:<br>";
-                        //display user account
-                        foreach ($user as $user_username => $user_password) {
-                            echo "Username: " . $user_username . "<br>";
-                            echo "Password: " . $user_password . "<br><br>";
+                        //check if user is admin
+                        if ($accounts[$username]['role'] == "admin"){
+                            echo "<br><h4>Welcome Admin</h4>";
+                            echo "User Account:<br>";
+                            //display user account
+                            foreach ($accounts as $user_username => $details) {
+                                if ($details['role'] == "user") {
+                                    echo "Username: " . $user_username . "<br>";
+                                    echo "Password: " . $details['password'] . "<br><br>";
+                                }
+                            }
+                        } else {
+                            echo "<br><h4>Welcome User</h4><br>";
                         }
-                    //check if username and password is valid
-                    } else if (array_key_exists($username, $user) && $password == $user[$username]){
-                        echo "<br>Login successful";
-                        echo "<br><h4>Welcome User</h4><br>";
                     } else {
                         echo "<br>Invalid username or password";
                     }
